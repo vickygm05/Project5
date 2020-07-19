b72472252677bbafbba300489bd5f338f929adca
vickygm05 <gmvicky5@gmail.com>
2020-07-19 18:37:23 +0530

    project

---
+++ src/test/java/org/tcs/com/Employee.java
@@ -0,0 +1,21 @@
+package org.tcs.com;
+
+public class Employee {
+
+	
+
+	public static void main(String[] args) {
+		
+		int a[][]=new int [2][3];
+		
+		a[0][0]=50;
+		a[0][1]=80;
+
+		a[1][0]=77;
+		a[1][1]=89;
+		a[1][2]=95;
+				
+		System.out.println(a[1][1]);
+		
+}
+}
