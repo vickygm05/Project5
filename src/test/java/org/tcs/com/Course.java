dcbba6893e0457d1320bd689a16211acd35119d2
vickygm05 <gmvicky5@gmail.com>
2020-07-21 10:07:45 +0530

    message

---
+++ src/test/java/org/tcs/com/Course.java
@@ -0,0 +1,25 @@
+package org.tcs.com;
+
+public class Course {
+int t =500;
+	
+	
+	public void add() {
+		
+		 int a =100;
+		
+		
+		System.out.println(t);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int b = 50;		
+		
+		
+			
+			
+
+}
+}
